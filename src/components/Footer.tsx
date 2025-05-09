6cf542eebb01d3733c91c2dce50d441af877ce69
Arvind <77539496+0x4rv1nd@users.noreply.github.com>
2025-05-10 00:05:00 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,34 +14,17 @@ const Footer = () => {
               <li><Link to="#" className="hover:text-wells-red">Savings Accounts</Link></li>
               <li><Link to="#" className="hover:text-wells-red">Credit Cards</Link></li>
               <li><Link to="#" className="hover:text-wells-red">Debit Cards</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Online Banking</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Mobile Banking</Link></li>
             </ul>
           </div>
           
-          {/* Column 2 */}
-          <div>
-            <h5 className="font-semibold mb-4">Loans & Credit</h5>
-            <ul className="space-y-2 text-sm">
-              <li><Link to="#" className="hover:text-wells-red">Mortgage</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Home Equity</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Personal Loans</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Auto Loans</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Student Loans</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Credit Management</Link></li>
-            </ul>
-          </div>
+        
           
           {/* Column 3 */}
           <div>
             <h5 className="font-semibold mb-4">Investing & Wealth</h5>
             <ul className="space-y-2 text-sm">
-              <li><Link to="#" className="hover:text-wells-red">Investment Services</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Retirement Planning</Link></li>
               <li><Link to="#" className="hover:text-wells-red">Financial Advisors</Link></li>
               <li><Link to="#" className="hover:text-wells-red">Trust Services</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Private Banking</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Estate Planning</Link></li>
             </ul>
           </div>
           
@@ -50,11 +33,8 @@ const Footer = () => {
             <h5 className="font-semibold mb-4">Help & Support</h5>
             <ul className="space-y-2 text-sm">
               <li><Link to="#" className="hover:text-wells-red">Customer Service</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Find Locations</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Security Center</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Fraud Information</Link></li>
               <li><Link to="#" className="hover:text-wells-red">Contact Us</Link></li>
-              <li><Link to="#" className="hover:text-wells-red">Careers</Link></li>
+             
             </ul>
           </div>
         </div>
@@ -75,14 +55,10 @@ const Footer = () => {
             <Link to="#" className="text-xs hover:text-wells-red">Privacy</Link>
             <Link to="#" className="text-xs hover:text-wells-red">Security</Link>
             <Link to="#" className="text-xs hover:text-wells-red">Terms & Conditions</Link>
-            <Link to="#" className="text-xs hover:text-wells-red">Accessibility</Link>
-            <Link to="#" className="text-xs hover:text-wells-red">Online Tracking & Advertising</Link>
           </div>
         </div>
         
-        <div className="mt-6 text-xs text-wells-darkgray">
-          <p>Important notice: This is a demonstration website for illustrative purposes only. This is not affiliated with Wells Fargo & Company.</p>
-        </div>
+        
       </div>
     </footer>
   );
